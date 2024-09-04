2e3edddfd29c9c594ab76b6b7727e06e9590969f
UrmiPatel-002772667 <129217613+UrmiPatel-002772667@users.noreply.github.com>
2024-09-04 11:31:46 -0400

    Create Student_enrollment.sql

---
+++ Structured Query Language/Dummy Data Files/Student_enrollment.sql
@@ -0,0 +1,21 @@
+create table student_enrollment
+(
+	student_no integer,
+	course_no varchar(5)
+);
+
+insert into student_enrollment values (1, 'CS110');
+insert into student_enrollment values (1, 'CS180');
+insert into student_enrollment values (1, 'CS210');
+insert into student_enrollment values (2, 'CS107');
+insert into student_enrollment values (2, 'CS220');
+insert into student_enrollment values (3, 'CS110');
+insert into student_enrollment values (3, 'CS180');
+insert into student_enrollment values (4, 'CS220');
+insert into student_enrollment values (5, 'CS110');
+insert into student_enrollment values (5, 'CS180');
+insert into student_enrollment values (5, 'CS210');
+insert into student_enrollment values (5, 'CS220');
+insert into student_enrollment values (6, 'CS110');
+insert into student_enrollment values (7, 'CS110');
+insert into student_enrollment values (7, 'CS210');

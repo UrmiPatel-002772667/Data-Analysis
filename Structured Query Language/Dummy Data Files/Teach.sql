1d6ccf449779b18e3936761e403720d12cfaa90c
UrmiPatel-002772667 <129217613+UrmiPatel-002772667@users.noreply.github.com>
2024-09-04 11:32:30 -0400

    Create Teach.sql

---
+++ Structured Query Language/Dummy Data Files/Teach.sql
@@ -0,0 +1,14 @@
+create table teach
+(
+	last_name varchar(20),
+	course_no varchar(5)
+);
+
+insert into teach values ('Chong', 'CS180');
+insert into teach values ('Brown', 'CS110');
+insert into teach values ('Brown', 'CS180');
+insert into teach values ('Jones', 'CS210');
+insert into teach values ('Jones', 'CS220');
+insert into teach values ('Wilson', 'CS110');
+insert into teach values ('Wilson', 'CS180');
+insert into teach values ('Williams', 'CS107');

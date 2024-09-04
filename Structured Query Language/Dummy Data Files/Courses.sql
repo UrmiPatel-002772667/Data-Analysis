e59d0edf47ec486ec8cca2b883404849c872bcec
UrmiPatel-002772667 <129217613+UrmiPatel-002772667@users.noreply.github.com>
2024-09-04 11:31:20 -0400

    Create Courses.sql

---
+++ Structured Query Language/Dummy Data Files/Courses.sql
@@ -0,0 +1,12 @@
+create table courses
+(
+	course_no varchar(5),
+	course_title varchar(20),
+	credits integer
+);
+
+insert into courses values ('CS110', 'Pre Calculus', 4);
+insert into courses values ('CS180', 'Physics', 4);
+insert into courses values ('CS107', 'Intro to Psychology', 3);
+insert into courses values ('CS210', 'Art History', 3);
+insert into courses values ('CS220', 'US History', 3);
